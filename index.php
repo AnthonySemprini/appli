dfece18d34d79e6072a915703ab6405f163617c9
Anthony <semprini.anthony@gmail.com>
2023-03-15 16:55:51 +0100

    css modifier sur les deux page et responcive jusqu'a 800px

--- index.php
+++ index.php
@@ -10,46 +10,50 @@
 </head>
 
 <body>
+
+    
     <div class="tab">
+        
 
         <h1 class="ajout">Ajouter un produit</h1>
+       
         <form action="traitement.php?action=add" method="post" class="form1" enctype="multipart/form-data">
-            <p>
+        <fieldset class="addProduit">
+            <legend> Produit </legend>
                 <label>
                     Nom du produit :
                     <input type="text" name="name" class="saisi">
                 </label>
-            </p>
-            <p>
+            
                 <label>
                     Prix du produit :
                     <input type="number" step="any" name="price" class="saisi" >
                 </label>
-            </p>
-            <p>
+            
                 <label>
                     Quantité désirée :
                     <input type="number" name="qtt" value="1" class="saisi">
                 </label>
-            </p>
-            <p>
+            
                 <label>
                     Description :
                     <textarea name="description" rows="5" cols="25" placeholder="Entrez votre texte..."></textarea>
                 </label>
-            </p>
-            <p>
-                <label for="file">Fichier</label>    
+                <div class="image">
+                <label for="file">Image du produit :</label>    
                 <input type="file" name="file" id="file">
-            </p>
-            <p>
-                <input type="submit" name="submit" value="Ajouter le produit" class="ajoutPanier">
-            </p>
+                </div>
+            </fieldset>
+            <div class="button">
+            <input type="submit" name="submit" value="Ajouter le produit" class="ajoutPanier">
+            <button class="myButton"><a href="recap.php">Panier</a></button>
+            <div/>
+           
+         
         </form>
 
         <br>
 
-        <button class="myButton"><a href="recap.php">Panier</a></button>
     </div>
 </body>
 
--- recap.php
+++ recap.php
@@ -12,75 +12,77 @@ session_start();
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Smokum&display=swap" rel="stylesheet">
-        <title>Réecapitulatif des produits</title>
+        <title>Récapitulatif des produits</title>
     </head>
 
     <body>
-        <div id=container>
-            <div class="tab">
-                <h1>Panier</h1>
+    <div id=container>
+            <div class="tab1">
+                        <h1 class="pan">Panier</h1>
 
-            <?php
-        if (!isset($_SESSION['products']) || empty($_SESSION['products'])) {
-                echo "<p>Aucun produit en session...</p>";
-                } else {
-        ?>   
+                        <?php
+                    if (!isset($_SESSION['products']) || empty($_SESSION['products'])) {
+                            echo "<p>Aucun produit en session...</p>";
+                            } else {
+                    ?>   
+                                
+                <table>
+                    <thead>",
+                        <tr>
+                            <th>#</th>
+                            <th>Nom</th>
+                            <th>Prix</th>
+                            <th>Quantité</th>
+                            <th>Description</th>
+                            <th>Total</th>
+                            <th>Photo</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+
+                        <?php
+                        $totalGeneral = 0;
+                        foreach ($_SESSION['products'] as $index => $product) {
+                        $total = $product['qtt'] * $product['price'];//ajout pour prendre en compte up et down qtt ?>
                         
-        <table>
-            <thead>",
-                <tr>
-                    <th>#</th>
-                    <th>Nom</th>
-                    <th>Prix</th>
-                    <th>Quantité</th>
-                    <th>Description</th>
-                    <th>Total</th>
-                    <th>Photo</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
 
-                <?php
-                $totalGeneral = 0;
-                foreach ($_SESSION['products'] as $index => $product) {
-                $total = $product['qtt'] * $product['price'];
-                //ajout pour prendre en compte up et down qtt ?>
+                    <tbody>
+                        <tr>
+                            <td><?= $index ?></td>
+                            <td><?= $product['name'] ?></td>
+                            <td><?= number_format($product['price'], 2, ",", "&nbsp;")?> &nbsp;€ </td>
+                            <td> <a href="traitement.php?action=up-qtt&id=<?= $index ?>"><i class="fa-solid fa-square-plus"></i></a>
+                                <?= $product['qtt'] ?>
+                                <a href="traitement.php?action=down-qtt&id=<?= $index ?>"><i class="fa-solid fa-square-minus"></i></a></td>
+                            <td><?= $product['description'] ?></td>
+                            <td><?= number_format($total, 2, ",", "&nbsp;") ?> &nbsp;€</td>
+                            <td>    <img src='upload/<?= $product['image'] ?>'>   </td>
+                            <td><a href='traitement.php?action=delete&id=<?= $index ?>'> <i class='fa-solid fa-trash-can'></a></i> </td>
+                        </tr>
 
-            <tbody>
-                <tr>
-                    <td><?= $index ?></td>
-                    <td><?= $product['name'] ?></td>
-                    <td><?= number_format($product['price'], 2, ",", "&nbsp;")?> . "&nbsp;€" </td>
-                    <td> <a href="traitement.php?action=up-qtt&id=<?= $index ?>">+</a>
-                        <?= $product['qtt'] ?>
-                        <a href="traitement.php?action=down-qtt&id=<?= $index ?>">-</a></td>
-                    <td><?= $product['description'] ?></td>
-                    <td><?= number_format($total, 2, ",", "&nbsp;") ?> . "&nbsp;€</td>
-                    <td>    <img src='upload/<?= $product['image'] ?>'>   </td>
-                    <td><a href='traitement.php?action=delete&id=<?= $index ?>'> <i class='fa-solid fa-trash-can'></a></i> </td>
-                </tr>
+                        <?php
+                        $totalGeneral += $total; 
+                        } //calcul le total general
+                        ?>
 
-                <?php
-                $totalGeneral += $total;  //calcul le total general
-                ?>
+                        <tr>
+                            <td colspan=4>Total général : </td>
+                            <td><strong><?= number_format($totalGeneral, 2, ", ", "&nbsp;")?>  &nbsp;€</strong></td>
+                        </tr>
+                    </tbody>
+                </table>
+            <?php
+            }
+            ?>
+                    <br>
+                    <div class="button">
 
-                <tr>
-                    <td colspan=4>Total général : </td>
-                    <td><strong>"<?= number_format($totalGeneral, 2, ", ", "&nbsp;")?> . "&nbsp;€ ?></strong></td>
-                </tr>
-            </tbody>
-        </table>
+                        <button class="myButton"><a href="traitement.php?action=deleteAll" method="get">vider panier</a></button>
 
+                        <button class="myButton"><a href="index.php">Ajouter produit</a></button>
+                    </div>
 
-            <br>
-            <div class="button">
-
-                <button class="myButton"><a href="traitement.php?action=deleteAll" method="get">vider panier</a></button>
-
-                <button class="myButton"><a href="index.php">Ajouter produit</a></button>
             </div>
-
-        </div>
         </div>
         <script src="https://kit.fontawesome.com/ef55713c5a.js" crossorigin="anonymous"></script>
     </body>
